f3f09eed6b62a67320b8c7fd57be509696af6a11
Hanzala-Sarang <Hanzalasarang01@gmail.com>
2023-09-28 21:19:52 +0530

    model trainer

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -4,6 +4,7 @@ from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
 
+from src.components.model_trainer import ModelTrainer
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 
@@ -15,3 +16,6 @@ if __name__ == "__main__":
     
     data_transformation = DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    
+    model_trainer=ModelTrainer()
+    model_trainer.initate_model_training(train_arr,test_arr)
