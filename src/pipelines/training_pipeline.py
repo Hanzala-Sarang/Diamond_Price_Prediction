6ee2c47eee8a9d1b0f045464c0f32b09e82a8fe1
Hanzala-Sarang <Hanzalasarang01@gmail.com>
2023-09-28 19:32:02 +0530

    Data Transformation completed

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     print(train_data_path,test_data_path)
     
     data_transformation = DataTransformation()
-    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
